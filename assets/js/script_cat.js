9abfa697b9d62c77751acf5b8b5bd83648f69d36
Eiki Yamashiro Santos <49211526+EikiSantos@users.noreply.github.com>
2019-09-16 19:02:51 -0300

    Java do Index feito

--- assets/js/script_cat.js
+++ assets/js/script_cat.js
@@ -1,32 +1,3 @@
-/*
-  É muito importante que todo seu código fique dentro deste
-  document.addEventListener, pois isso garante que ele vai
-  rodar apenas quando toda a página estiver carregada. Isso
-  foi explicado no Material Prévio de JavaScript da Aula 9.
-*/
-/* document.addEventListener('DOMContentLoaded', function() {
-
-  /*
-    Neste exemplo, por simplicidade, vamos usar um dicionário
-    hardcoded. No projeto, os dados devem ser construídos a
-    partir dos dados baixados do Firebase, como vimos antes.
-  */
-/*  let cat = coDesConnect('https://comp1mec-80273.firebaseio.com/')
-
-  /*
-    A função coDesReplace recebe dois parâmetros. O primeiro
-    é um seletor para localizar algum elemento da página. No
-    exemplo abaixo, usamos ".list" para localizar o elemento
-    cuja classe é "list". Não esqueça que é como no CSS: os
-    nomes de classes devem começar com ponto.
-
-    O segundo é um dicionário cujas chaves são palavras que
-    estão entre {{ e }} nesse elemento e cujos valores são os
-    textos que devem substituir essas ocorrências de {{}}.
-  */
-/*  coDesReplace('.list', cat)
-}) */
-
 document.addEventListener('DOMContentLoaded', function() {
   let params = coDesExtract()
   let value = params['key']
@@ -34,27 +5,11 @@ document.addEventListener('DOMContentLoaded', function() {
   let cat = coDesConnect('https://comp1mec-80273.firebaseio.com/')
 
   cat.download('/', function(data) {
-
-    /*
-      NUNCA, JAMAIS, EM HIPÓTESE ALGUMA faça
-      replace em múltiplas tags se alguma
-      estiver dentro de outra! Veja o HTML
-      e note que não é o caso das abaixo.
-
-      É sério. Se fizer isso, você realmente
-      vai se arrepender. Você foi avisado.
-    */
-
     context = data['portfolio'][value]
     coDesReplace('title', context)
-
     context = data
-    coDesReplace('.menu-list', context)
-
-    context = data['portfolio'][value]
-    coDesReplace('.call', context)
-
-    context = data['portfolio'][value]
-    coDesReplace('.book-list', context)
+    coDesReplace('.main-menu', context)
+    context = data['portifolio'][value][projetos]
+    coDesReplace('.software-sections', context)
   })
 })
--- assets/js/script_ind.js
+++ assets/js/script_ind.js
@@ -1,38 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
- /* let params = coDesExtract()
-  let value = params['key'] */
-
+  let params = coDesExtract()
+  let value = params['key']
   let db = coDesConnect('https://comp1mec-80273.firebaseio.com/')
 
   db.download('/', function(data) {
-
-    /*
-      NUNCA, JAMAIS, EM HIPÓTESE ALGUMA faça
-      replace em múltiplas tags se alguma
-      estiver dentro de outra! Veja o HTML
-      e note que não é o caso das abaixo.
-
-      É sério. Se fizer isso, você realmente
-      vai se arrepender. Você foi avisado.
-    */
-
     context = data
     coDesReplace('.index-sections', context)
-
-    /*
     context = data
-    console.log(data)
-<<<<<<< HEAD
-    console.log(data['biblioteca'][value])
-   
-    coDesReplace('.menu-list', context)
-
-    context = data['biblioteca'][value]
-    coDesReplace('.call', context)
-
-    context = data['biblioteca'][value]
-    coDesReplace('.book-list', context)
-
-   */
+    coDesReplace('.main-menu', context)
   })
 })
