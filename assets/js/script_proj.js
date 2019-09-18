28a3aecdc30a6e5dc6a7eef7b561cb1806f3233b
williamars <williamars@al.insper.edu.br>
2019-09-18 02:12:52 -0300

    Finalizando o projeto

--- assets/js/script_proj.js
+++ assets/js/script_proj.js
@@ -1,18 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
   let params = coDesExtract()
   let value = params['key']
+  let categ = params['categ']
+
+
   let db = coDesConnect('https://comp1mec-80273.firebaseio.com/')
 
   db.download('/', function(data) {
     context = data
     coDesReplace('.main-menu', context)
 
-    context = data['portfolio']
+    context = data['portfolio'][categ]['projetos'][value]
+    coDesReplace('.blabla', context)
+
+    context = data['portfolio'][categ]['projetos'][value]
     coDesReplace('.proj-title', context)
 
-    context = data['portfolio'][value]
+    context = data['portfolio'][categ]['projetos'][value]
     coDesReplace('.proj-eq-mat', context)
-   
+
+    context = data['portfolio'][categ]['projetos'][value]
+    coDesReplace('.blabla2', context)
+
+    context = data['portfolio'][categ]['projetos'][value]
+    coDesReplace('.proj-subtitle', context)
 
   })
 })
