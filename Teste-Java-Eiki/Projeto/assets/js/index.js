de2cba18e8a5f30e50b459e961ce6e98fde877c8
Eiki Yamashiro Santos <49211526+EikiSantos@users.noreply.github.com>
2019-09-18 00:09:48 -0300

    Deletei as pastas inuteis

--- Teste-Java-Eiki/Projeto/assets/js/index.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  let db = coDesConnect('https://codesigndeaplicativos.firebaseio.com/')
-
-  db.download('/', function(data) {
-    context = data
-    coDesReplace('.menu-list', context)
-    coDesReplace('.section-list', context)
-  })
-})
